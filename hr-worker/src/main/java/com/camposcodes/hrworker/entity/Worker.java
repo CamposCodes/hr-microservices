c2223ccf82178da28291685ad202b589b8ebefff
CamposCodes <campos.gabriel@estudante.ufjf.br>
2026-08-31 03:34:12 -0300

    style: cleanup hr-worker formatting and add serialVersionUID to Worker

--- hr-worker/src/main/java/com/camposcodes/hrworker/entity/Worker.java
+++ hr-worker/src/main/java/com/camposcodes/hrworker/entity/Worker.java
@@ -7,12 +7,12 @@ import java.io.Serializable;
 @Table(name = "tb_worker")
 public class Worker implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
     private Double dailyIncome;
 
     public Worker(){
